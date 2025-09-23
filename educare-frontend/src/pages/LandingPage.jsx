9aeeec3a678d726817f8fcb26b41c76925f915a6
Nirmada <nirmada0504@gmail.com>
2025-09-23 12:15:35 +0530

    Initial commit - setup EduCare frontend with Vite + React

---
+++ educare-frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <section>
+            <h1>Welcome to EduCare</h1>
+            <p>Your one-stop solution for all educational resources.</p>
+        </section>
+    </div>
+  )
+}
+
+export default LandingPage
