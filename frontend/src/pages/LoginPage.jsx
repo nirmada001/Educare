0f3a809672c0a211de7b4ab99b36e98d8cd3b8ba
Nirmada <nirmada0504@gmail.com>
2025-10-09 15:41:33 +0530

    Changed to tailwind CSS

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-blue-200">
+      <h1 className="text-4xl font-bold text-blue-700">Tailwind Works! 🎉</h1>
+    </div>
+  );
+}
